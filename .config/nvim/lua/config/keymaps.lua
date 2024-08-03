4aac32bc893d4c6699fbede67d050736325caa90
kenjiebalona <kbalona5@gmail.com>
2024-08-03 14:56:52 +0800

    add neovim

---
+++ .config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,3 @@
+-- keymaps are automatically loaded on the verylazy event
+-- default keymaps that are always set: https://github.com/lazyvim/lazyvim/blob/main/lua/lazyvim/config/keymaps.lua
+-- add any additional keymaps here
