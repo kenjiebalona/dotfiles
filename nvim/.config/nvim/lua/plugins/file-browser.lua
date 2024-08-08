e19e2b970f03629959f914c094b77369af8b9ef0
kenjiebalona <kbalona5@gmail.com>
2024-08-09 04:32:05 +0800

    add telescope file-browser

---
+++ nvim/.config/nvim/lua/plugins/file-browser.lua
@@ -0,0 +1,65 @@
+return {
+  "telescope.nvim",
+  dependencies = {
+    "nvim-telescope/telescope-file-browser.nvim",
+  },
+  keys = {
+    {
+      "<leader>sf",
+      function()
+        local telescope = require("telescope")
+
+        local function telescope_buffer_dir()
+          return vim.fn.expand("%:p:h")
+        end
+
+        telescope.extensions.file_browser.file_browser({
+          path = "%:p:h",
+          cwd = telescope_buffer_dir(),
+          respect_gitignore = false,
+          hidden = true,
+          grouped = true,
+          previewer = false,
+          initial_mode = "normal",
+          layout_config = { height = 40 },
+        })
+      end,
+      desc = "Open File Browser with the path of the current buffer",
+    },
+  },
+  config = function(_, opts)
+    local telescope = require("telescope")
+
+    opts.defaults = vim.tbl_deep_extend("force", opts.defaults, {
+      wrap_results = true,
+      layout_strategy = "horizontal",
+      layout_config = { prompt_position = "top" },
+      sorting_strategy = "ascending",
+      winblend = 0,
+      mappings = {
+        n = {},
+      },
+    })
+
+    opts.picker = {
+      diagnostics = {
+        theme = "ivy",
+        initial_mode = "normal",
+        layout_config = {
+          preview_cutoff = 9999,
+        },
+      },
+    }
+
+    opts.extensions = {
+      file_browser = {
+        theme = "dropdown",
+        -- disables netrw and use telescope-file-browser in its place
+        hijack_netrw = true,
+      },
+    }
+    telescope.setup(opts)
+    require("telescope").load_extension("fzf")
+    require("telescope").load_extension("file_browser")
+  end,
+}
