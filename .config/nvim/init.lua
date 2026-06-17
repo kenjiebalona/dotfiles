5f250a013a706c64ed63924a32699686052a4efb
kenjiebalona <kbalona5@gmail.com>
2026-06-17 16:54:34 +0800

    restructure layout into flat directory

---
+++ .config/nvim/init.lua
@@ -0,0 +1,11 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- TRANSPARENT nvim
+vim.cmd [[
+  highlight Normal guibg=none
+  highlight NonText guibg=none
+  highlight Normal ctermbg=none
+  highlight NonText ctermbg=none
+]]
+
