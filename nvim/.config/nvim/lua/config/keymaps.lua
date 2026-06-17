b79d02c9aff3b28020e31cdf5126512bb7f93603
kenjiebalona <kbalona5@gmail.com>
2026-06-17 15:18:02 +0800

    remove nvim

--- nvim/.config/nvim/lua/config/keymaps.lua
+++
@@ -1,5 +0,0 @@
--- keymaps are automatically loaded on the verylazy event
--- default keymaps that are always set: https://github.com/lazyvim/lazyvim/blob/main/lua/lazyvim/config/keymaps.lua
--- add any additional keymaps here
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
